7a559fd60554d7d9428600fec606c41ba2f220d3
Desenvolvimento <repository.desenvolvimento@gmail.com>
2021-01-24 15:14:07 -0300

    PLGKABYLEAPI-09243701202110

--- src/main/java/br/com/plataformalancamento/application/repository/LivroRepository.java
+++ src/main/java/br/com/plataformalancamento/application/repository/LivroRepository.java
@@ -2,8 +2,10 @@ package br.com.plataformalancamento.application.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import br.com.plataformalancamento.application.model.LivroModel;
 
+@CrossOrigin("http://localhost:8080")
 @RepositoryRestResource(collectionResourceRel = "livroList", path = "livro")
 public interface LivroRepository extends JpaRepository<LivroModel, Long> { }
