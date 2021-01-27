3700790d03210279701a795e6b95db886bf93276
Desenvolvimento <repository.desenvolvimento@gmail.com>
2021-01-27 15:49:56 -0300

    PLGKABYLEAPI-08262701202119

--- src/main/java/br/com/plataformalancamento/application/repository/LivroRepository.java
+++ src/main/java/br/com/plataformalancamento/application/repository/LivroRepository.java
@@ -1,6 +1,9 @@
 package br.com.plataformalancamento.application.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -8,4 +11,8 @@ import br.com.plataformalancamento.application.model.LivroModel;
 
 @CrossOrigin("http://localhost:8080")
 @RepositoryRestResource(collectionResourceRel = "livroList", path = "livro")
-public interface LivroRepository extends JpaRepository<LivroModel, Long> { }
+public interface LivroRepository extends JpaRepository<LivroModel, Long> {
+	
+	public Page<LivroModel> findByCategoriaLivroModelCodigo(@Param("codigo") Long codigo, Pageable pageable);
+	
+}
