b49beb9fd1370cc437d06d0d284cc72158510c5f
Desenvolvimento <repository.desenvolvimento@gmail.com>
2021-01-27 20:02:10 -0300

    PLGKABYLEAPI-08262701202119

--- src/main/java/br/com/plataformalancamento/application/repository/LivroRepository.java
+++ src/main/java/br/com/plataformalancamento/application/repository/LivroRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import br.com.plataformalancamento.application.model.LivroModel;
@@ -13,6 +14,7 @@ import br.com.plataformalancamento.application.model.LivroModel;
 @RepositoryRestResource(collectionResourceRel = "livroList", path = "livro")
 public interface LivroRepository extends JpaRepository<LivroModel, Long> {
 	
+	@RestResource(path = "categoria")
 	public Page<LivroModel> findByCategoriaLivroModelCodigo(@Param("codigo") Long codigo, Pageable pageable);
 	
 }
--- src/main/resources/plgkabyleapi.sql
+++ src/main/resources/plgkabyleapi.sql
@@ -1,4 +1,9 @@
-INSERT INTO PUBLIC.TB_CATEGORIA_LIVRO (CODIGO, DESCRICAO) VALUES (NEXTVAL ('PUBLIC.HIBERNATE_SEQUENCE'), 'Programação');
+INSERT INTO PUBLIC.TB_CATEGORIA_LIVRO (CODIGO, DESCRICAO) VALUES
+(NEXTVAL ('PUBLIC.HIBERNATE_SEQUENCE'), 'Programação'),
+(NEXTVAL ('PUBLIC.HIBERNATE_SEQUENCE'), 'Banco de Dados'),
+(NEXTVAL ('PUBLIC.HIBERNATE_SEQUENCE'), 'Literatura'),
+(NEXTVAL ('PUBLIC.HIBERNATE_SEQUENCE'), 'Infraestrutura');
+
 INSERT INTO PUBLIC.TB_LIVRO (codigo, DATA_ATUALIZACAO_REGISTRO, DATA_CRIACAO_REGISTRO, DESCRICAO, IS_ATIVO, NOME, PRECO_UNITARIO, QUANTIDADE_ESTOQUE, SKU, URL_IMAGEM, USUARIO_ULTIMA_ALTERACAO, ID_CATEGORIA_LIVRO) VALUES 
 (nextval ('public.hibernate_sequence'), NOW(), NOW(), '', true, 'Web Scraping com Python: Coletando Mais Dados da web Moderna', 67.70, 10, 'LV_0002_0002', 'https://images-na.ssl-images-amazon.com/images/I/41gx74x+gdL._SX347_BO1,204,203,200_.jpg', '000.000.000-00', 1),
 (nextval ('public.hibernate_sequence'), NOW(), NOW(), '', true, 'Estruturas de Dados e Algoritmos com JavaScript: Escreva um Código JavaScript Complexo e Eficaz Usando a Mais Recente ECMAScript ', 71.91, 10, 'LV_0002_0002', '', '000.000.000-00', 1),
@@ -10,3 +15,8 @@ INSERT INTO PUBLIC.TB_LIVRO (codigo, DATA_ATUALIZACAO_REGISTRO, DATA_CRIACAO_REG
 (nextval ('public.hibernate_sequence'), NOW(), NOW(), '', true, 'Algoritmos Funcionais: Introdução Minimalista à Lógica de Programação Funcional Pura Aplicada à Teoria dos Conjuntos', 34.90, 10, 'LV_0002_0002', 'https://images-na.ssl-images-amazon.com/images/I/51nObPCqLcL._SX357_BO1,204,203,200_.jpg', '000.000.000-00', 1),
 (nextval ('public.hibernate_sequence'), NOW(), NOW(), '', true, 'React - Aprenda Praticando: Desenvolva Aplicações web Reais com uso da Biblioteca React e de Seus Módulos Auxiliares', 69.00, 10, 'LV_0002_0002', 'https://images-na.ssl-images-amazon.com/images/I/51ATqZkXSzL._SX363_BO1,204,203,200_.jpg', '000.000.000-00', 1),
 (nextval ('public.hibernate_sequence'), NOW(), NOW(), '', true, 'Python Fluente: Programação Clara, Concisa e Eficaz', 109.99, 10, 'LV_0002_0002', 'https://images-na.ssl-images-amazon.com/images/I/51Ov4P3XiEL._SX357_BO1,204,203,200_.jpg', '000.000.000-00', 1);
+(nextval ('public.hibernate_sequence'), NOW(), NOW(), '', true, 'MySQL: Comece com o principal banco de dados open source do mercado', 35.91, 10, 'LV_0002_0002', 'https://m.media-amazon.com/images/I/41sO3IBUXVL.jpg', '000.000.000-00', 24),
+(nextval ('public.hibernate_sequence'), NOW(), NOW(), '', true, 'Oracle Database 12c Security Cookbook (English Edition)', 136.99, 10, 'LV_0002_0002', 'https://m.media-amazon.com/images/I/515kI4kRUWL.jpg', '000.000.000-00', 24),
+(nextval ('public.hibernate_sequence'), NOW(), NOW(), '', true, 'Manual de DevOps: Como Obter Agilidade, Confiabilidade e Segurança em Organizações Tecnológicas', 75.20, 10, 'LV_0002_0002', 'https://images-na.ssl-images-amazon.com/images/I/51HyO3cPUiL._SX351_BO1,204,203,200_.jpg', '000.000.000-00', 27),
+(nextval ('public.hibernate_sequence'), NOW(), NOW(), '', true, 'O Projeto Fênix: um Romance Sobre TI, DevOps e Sobre Ajudar o seu Negócio a Vencer', 54.99, 10, 'LV_0002_0002', 'https://images-na.ssl-images-amazon.com/images/I/51QanGkqphL._SX348_BO1,204,203,200_.jpg', '000.000.000-00', 27);
+
